81e7e2b1f67a09e797fb05d1558822fec1f603b9
haruhixkti <haruhixkti@gmail.com>
2017-09-19 16:13:46 -0400

    ActivityRender agregado

---
+++ src/main/java/activityRender/ActivityRender.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package activityRender;
+
+/**
+ *
+ * @author Katherine
+ */
+import static activityRender.Recorder.captureInterval;
+import static activityRender.Recorder.makeVideo;
+import static activityRender.Recorder.record;
+import static activityRender.Recorder.screenHeight;
+import static activityRender.Recorder.screenWidth;
+import static activityRender.Recorder.startRecord;
+import static activityRender.Recorder.store;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.util.Scanner;
+import java.util.Vector;
+
+import javax.imageio.ImageIO;
+import javax.media.MediaLocator;
+import principal.Pestanas;
+public class ActivityRender {
+    /**
+	 * Screen Width.
+	 */
+	public static int screenWidth = (int) Toolkit.getDefaultToolkit()
+			.getScreenSize().getWidth();
+
+	/**
+	 * Screen Height.
+	 */
+	public static int screenHeight = (int) Toolkit.getDefaultToolkit()
+			.getScreenSize().getHeight();
+
+	/**
+	 * Interval between which the image needs to be captured.
+	 */
+	public static int captureInterval = 1000;
+
+	/**
+	 * Temporary folder to store the screenshot.
+	 */
+	public static String store = "tmp";
+
+	/**
+	 * Status of the recorder.
+	 */
+	public static boolean record = false;
+
+	/**
+	 * 
+	 */
+        	public static void startRecord() {
+		Thread recordThread = new Thread() {
+			@Override
+			public void run() {
+				Robot rt;
+				int cnt = 0;
+				try {
+					rt = new Robot();
+					while (cnt == 0 || record) {
+						BufferedImage img = rt
+								.createScreenCapture(new Rectangle(screenWidth,
+										screenHeight));
+						ImageIO.write(img, "jpeg", new File("./"+store+"/"
+								+ System.currentTimeMillis() + ".jpeg"));
+						if (cnt == 0) {
+							record = true;
+							cnt = 1;
+						}
+						// System.out.println(record);
+						Thread.sleep(captureInterval);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		recordThread.start();
+	}
+    	public static void makeVideo(String movFile) throws MalformedURLException {
+		System.out
+				.println("#### Easy Capture making video, please wait!!! ####");
+		JpegImagesToMovie imageToMovie = new JpegImagesToMovie();
+		Vector<String> imgLst = new Vector<String>();
+		File f = new File(store);
+		File[] fileLst = f.listFiles();
+		for (int i = 0; i < fileLst.length; i++) {
+			imgLst.add(fileLst[i].getAbsolutePath());
+		}
+		// Generate the output media locators.
+		MediaLocator oml;
+		if ((oml = imageToMovie.createMediaLocator(movFile)) == null) {
+			System.err.println("Cannot build media locator from: " + movFile);
+			System.exit(0);
+		}
+		imageToMovie.doIt(screenWidth, screenHeight, (1000 / captureInterval),
+				imgLst, oml);
+
+	}
+        public void test(){
+            System.out.println("entre");
+        }
+        
+        public void inicializar (String path, int numeroMuestra ) throws Exception{
+        System.out.println("######### Starting Easy Capture Recorder #######");
+        	Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+		System.out.println("Your Screen [Width,Height]:" + "["
+				+ screen.getWidth() + "," + screen.getHeight() + "]");
+		
+              /**  for(int i=0;i<5;i++){
+			System.out.print(".");
+			Thread.sleep(1000);
+		}**/
+		File f = new File(store);
+		if(!f.exists()){
+			f.mkdir();
+		}
+                startRecord();
+                Pestanas.iPerspectivaActivityRender= true;
+                System.out.println("Grabando");
+        }
+        public void detener() throws MalformedURLException{
+        
+        record = false;
+		System.out.println("Easy Capture has stopped.");
+		makeVideo(System.currentTimeMillis()+".mov");
+        Pestanas.dPerspectivaActivityRender = true;
+        }
+}
