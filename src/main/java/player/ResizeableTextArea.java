f9fb14af434b0b6df4603548d72a2efa593333da
haruhixkti <haruhixkti@gmail.com>
2017-10-04 20:49:17 -0300

    dragable

---
+++ src/main/java/player/ResizeableTextArea.java
@@ -0,0 +1,187 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package player;
+
+/**
+ *
+ * @author Katherine
+ */
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.text.DefaultCaret;
+@SuppressWarnings("serial")
+public class ResizeableTextArea extends JPanel  {
+   //tamaño de la pantalla inicial
+    private static final int PREF_WIDTH = 700;
+   private static final int PREF_HEIGHT = 500;
+   
+   private static final int ROWS = 60;
+   private static final int COLS = 80;
+   //color del borde del cuadrado (antes de soltar el click)
+   private static final Color RECT_COLOR = new Color(102,255,255);
+   private JLabel textArea = new JLabel();
+   //private JTextArea textArea = new JTextArea("tag",ROWS, COLS);
+  
+   private JScrollPane scrollPane = new JScrollPane(textArea);
+   private int x, y, width, height;
+   private boolean drawRect = false;
+
+   public ResizeableTextArea() {
+       System.out.println("ResizeableTextArea()");
+      setLayout(null);
+      add(scrollPane);
+
+      MyMouseAdapter myMouseAdapter = new MyMouseAdapter(); 
+      addMouseListener(myMouseAdapter);
+      addMouseMotionListener(myMouseAdapter);
+   }
+   @Override
+   protected void paintComponent(Graphics g) {
+       System.out.println("CUARTO EVENTO: paintComponent");
+       //el cuadrado que se muestra antes de que aparezca el jtext
+      super.paintComponent(g);
+      if (drawRect) {
+          System.out.println(" iF paintComponent IF");
+         g.setColor(RECT_COLOR);
+         g.drawRect(x, y, width, height);         
+      }
+   }
+     @Override
+   public Dimension getPreferredSize() {
+         System.out.println("getPreferredSize");
+      return new Dimension(PREF_WIDTH, PREF_HEIGHT);
+   }
+    private class MyMouseAdapter extends MouseAdapter {
+        
+      private int innerX, innerY;
+
+      @Override
+      public void mousePressed(MouseEvent e) {
+           System.out.println("PRIMER EVENTO: mousePressed");
+         //SE CONSIGUE LA POSICIÓN DEL MOUSE (ACA PODRIA LIMITAR EL ESPACIO)
+         x = e.getX();
+         y = e.getY();
+         innerX = x;
+         innerY = y;
+         width = 0;
+         height = 0;
+         
+         drawRect = true;
+      }
+
+      @Override
+      public void mouseDragged(MouseEvent e) {
+          System.out.println("mouseDragged");
+         calcBounds(e);
+
+         drawRect = true;
+         ResizeableTextArea.this.repaint();
+      }
+
+      @Override
+      public void mouseReleased(MouseEvent e) {
+          System.out.println("TERCER EVENTO EVENTO: mouseReleased");
+          
+         calcBounds(e);
+        //cuando el mouse se suelta
+         drawRect = false;
+         
+         scrollPane.setBounds(x, y, width, height);
+         
+         scrollPane.getViewport().revalidate();
+         ResizeableTextArea.this.repaint();
+         textArea.setText("tag");
+         textArea.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+         //textArea.setEditable(false);
+         textArea.setBackground(new java.awt.Color(204,204,255));
+         textArea.setOpaque(true);
+         //textArea.setLineWrap(true);
+         //textArea.setWrapStyleWord(true);
+         
+         
+          System.out.println("TEXTO: "+ textArea.getText());
+
+      }
+
+      private void calcBounds(MouseEvent e) {
+          System.out.println("SEGUNDO EVENTO: calcBounds");
+       //ancho
+       
+       
+       //largo
+       
+          
+          
+         //largo del cuadro
+         if(Math.min(innerY, e.getY()) + width > PREF_WIDTH){
+         height = 49;
+         y= PREF_WIDTH-49;
+         }
+         else{
+          height = 49;
+         }
+         
+         //height = Math.abs(innerY - e.getY());
+          //  PREF_WIDTH = 700; ancho (eje y)
+         //PREF_HEIGHT = 500; largo ( eje x)
+         
+         if (Math.min(innerX, e.getX()) < 0){
+         x = 0;
+         }
+         else if(Math.min(innerX, e.getX())>PREF_HEIGHT){
+         x = PREF_HEIGHT;
+         }
+         else{
+         x = Math.min(innerX, e.getX()); 
+         }
+         
+            //ancho del cuadro
+        if( Math.min(innerX, e.getX()) + width >PREF_WIDTH){
+        width=PREF_WIDTH-x;
+        }
+        else{ width = Math.abs(innerX - e.getX());}
+        
+        
+        if(Math.min(innerY, e.getY())<0){
+            y = 0;
+        }
+        else if (Math.min(innerY, e.getY()) + width > PREF_WIDTH){
+            y = PREF_WIDTH;
+        }
+        else {
+        y = Math.min(innerY, e.getY());
+        }
+         
+         
+   
+          /*System.out.println("width: "+ width);
+          System.out.println("height: "+ height);
+          
+          System.out.println("x:"+x);
+          System.out.println("y: "+ y);*/
+      }
+
+   }
+
+   private static void createAndShowUI() {
+       System.out.println("createAndShowUI");
+      JFrame frame = new JFrame("ResizeableTextArea");
+      frame.getContentPane().add(new ResizeableTextArea());
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.pack();
+      frame.setLocationRelativeTo(null);
+      frame.setVisible(true);
+   }
+
+   public static void main(String[] args) {
+      java.awt.EventQueue.invokeLater(new Runnable() {
+         public void run() {
+            createAndShowUI();
+         }
+      });
+   }
+}
